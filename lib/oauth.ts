636cc94b7373c149946686749f3d84b1408ef895
Avitus Ibrahim <avitussweetbert213@gmail.com>
2024-09-18 02:01:07 +0300

    redirect uri added

--- lib/oauth.ts
+++ lib/oauth.ts
@@ -3,13 +3,13 @@ import { Google, GitHub } from "arctic";
 export const google = new Google(
   process.env.GOOGLE_CLIENT_ID!,
   process.env.GOOGLE_CLIENT_SECRET!,
-  process.env.GOOGLE_REDIRECT_URI!
+  "https://video-sharing-gamma.vercel.app/api/oauth/google"
 );
 
 export const github = new GitHub(
   process.env.GITHUB_CLIENT_ID!,
   process.env.GITHUB_CLIENT_SECRET!,
   {
-    redirectURI: process.env.GITHUB_REDIRECT_URI!,
+    redirectURI: "https://video-sharing-gamma.vercel.app/api/oauth/github",
   }
 );
