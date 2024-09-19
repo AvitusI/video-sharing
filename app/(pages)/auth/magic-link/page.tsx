5ffe3995d2b97ff94f1a71e64d990a4805507006
Avitus Ibrahim <avitussweetbert213@gmail.com>
2024-09-19 16:58:40 +0300

    styled

--- app/(pages)/auth/magic-link/page.tsx
+++ app/(pages)/auth/magic-link/page.tsx
@@ -3,7 +3,7 @@ import { MagicLinkForm } from "@/app/components/MagicLinkForm";
 export default function Page() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="w-[360px] flex flex-col gap-2">
+      <div className="w-[360px] flex flex-col gap-4 p-2">
         <span className="text-2xl font-light text-center">
           One Time Sign In
         </span>
